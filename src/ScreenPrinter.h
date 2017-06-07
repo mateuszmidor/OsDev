0f66bffca3839fbc5016bf8fa60c0d2729ee5adf
Mateusz Midor <3demaniac@gmail.com>
2017-06-07 14:09:01 +0200

    ScreenPrinter.format() now handles integers

--- src/ScreenPrinter.h
+++ src/ScreenPrinter.h
@@ -33,6 +33,58 @@ public:
     void set_bg_color(const Color value);
     void clearscreen();
 
+    void format(long long int num) {
+        int base = 10;
+        char str[12];
+
+            int i = 0;
+            bool isNegative = false;
+
+            /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+            if (num == 0)
+            {
+                format("0");
+                return ;
+            }
+
+            // In standard itoa(), negative numbers are handled only with
+            // base 10. Otherwise numbers are considered unsigned.
+            if (num < 0 && base == 10)
+            {
+                isNegative = true;
+                num = -num;
+            }
+
+            // Process individual digits
+            while (num != 0)
+            {
+                int rem = num % base;
+                str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+                num = num/base;
+            }
+
+            // If number is negative, append '-'
+            if (isNegative)
+                str[i++] = '-';
+
+            str[i] = '\0'; // Append string terminator
+
+            // Reverse the string
+            int start = 0;
+            int end = i -1;
+            while (start < end)
+            {
+
+                auto tmp = *(str+start);
+                *(str+start) = *(str+end);
+                *(str+end) = tmp;
+                start++;
+                end--;
+            }
+
+        format(str);
+    }
+
     void format(char const *fmt) {
         while (*fmt)
             putc(*fmt++);
