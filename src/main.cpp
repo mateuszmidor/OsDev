22c546bf0981a140b877c4f2bf305b7d2579ff07
Mateusz Midor <3demaniac@gmail.com>
2017-06-13 10:45:48 +0200

    kmain now runs constructors of global defined objects

--- src/main.cpp
+++ src/main.cpp
@@ -33,20 +33,38 @@ void test_kstd(ScreenPrinter &p) {
 //    p.format("%\n", s2.c_str());
 }
 
+/**
+ * @name    callGlobalConstructors
+ * @brief   Call the constructors of objects defined in global namespace (if any)
+ * @note    start_ctors and end_ctors defined in linker.ld
+ */
+typedef void (*Constructor)();
+extern "C" Constructor start_ctors;
+extern "C" Constructor end_ctors;
+void callGlobalConstructors() {
+    for (Constructor* c = &start_ctors; c != &end_ctors; c++)
+        (*c)();
+}
 
 /**
  * kmain
  * Kernel entry point
  */
 extern "C" void kmain(void *multiboot2_info_ptr) {
+    // run constructors of global objects. This could be run from long_mode_init.S
+    callGlobalConstructors();
+
+    // print hello message to the user
     ScreenPrinter p;
     p.set_bg_color(Color::Blue);
     p.format("\n\n"); // go to the third line of console as 1 and 2 are used upon initializing in 32 and then 64 bit mode
     p.format("Hello in kmain() of main.cpp!\n");
 
+    // print CPU info
     CpuInfo cpu_info;
     cpu_info.print(p);
 
+    // print Multiboot2 info related to framebuffer config, available memory and kernel ELF sections
     Multiboot2 mb2(multiboot2_info_ptr);
     mb2.print(p);
 
