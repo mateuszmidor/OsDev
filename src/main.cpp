9a7c21e8dca260a5a47ca7a233bbffc5ee3d62db
Mateusz Midor <3demaniac@gmail.com>
2017-06-14 10:12:43 +0200

    Implemented CPU exception handling for zero division and breakpoint

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,14 @@
 #include "Multiboot2.h"
 #include "CpuInfo.h"
 #include "kstd.h"
+#include "types.h"
 
 
 using namespace kstd;
 
+/**
+ * Test kstd namespace functionality
+ */
 void test_kstd(ScreenPrinter &p) {
     // test vector
     vector<long long> vec;
@@ -23,16 +27,26 @@ void test_kstd(ScreenPrinter &p) {
 
     p.format("\n");
 
-    // test sting
+    // test string
     string s("abcdefghijklkmnoprstuwxyz");
     s += "123456abcdefghijklkmnoprstuwxyz";
     p.format("%\n", s.c_str());
 
     // test flags
-//    string s2= flags_to_str(5, "READ=0x4", "WRITE=0x2", "EXEC=0x1");
-//    p.format("%\n", s2.c_str());
+    string s2= flags_to_str(5, "READ=0x4", "WRITE=0x2", "EXEC=0x1");
+    p.format("%\n", s2.c_str());
 }
 
+/**
+ * Test Interrupt Descriptor Table
+ */
+void test_idt() {
+    // check breakpoint exception handler
+    asm("int3");
+
+    // check zero division exception handler
+    asm("xor %rbx, %rbx; idiv %rbx");
+}
 /**
  * @name    callGlobalConstructors
  * @brief   Call the constructors of objects defined in global namespace (if any)
@@ -47,12 +61,95 @@ void callGlobalConstructors() {
 }
 
 /**
+ * Interrupt Descriptor Table Entry Options
+ */
+struct IdtEntryOptions {
+    u16 interrupt_stack_table_index : 3;    // 0 for DONT CHANGE THE STACKS
+    u16 reserved                    : 5;    // always 0
+    u16 interrupts_enabled          : 1;    // better be 0 for now
+    u16 always_1                    : 3;    // always 0b111
+    u16 always_0                    : 1;    // always 0
+    u16 min_privilege_level         : 2;    // 3 for user space
+    u16 present                     : 1;    // 1 for true
+
+    IdtEntryOptions(bool is_present = false) {
+        interrupt_stack_table_index = 0;
+        reserved = 0;
+        interrupts_enabled = 0;
+        always_1 = 7; // 0b111
+        always_0 = 0;
+        min_privilege_level = 3;
+        present = is_present;
+
+    }
+} __attribute__((packed));
+
+/**
+ * Interrupt Descriptor Table Entry
+ */
+struct IdtEntry {
+    u16 pointer_low;
+    u16 gdt_code_segment_selector;  // offset of code segment in GDT, in our case 8
+    IdtEntryOptions options;
+    u16 pointer_middle;
+    u32 pointer_high;
+    u32 always_0;
+
+} __attribute__((packed));
+
+IdtEntry make_entry(u64 pointer) {
+    IdtEntry e;
+
+    e.gdt_code_segment_selector = 8; // at offset 8 starts our one and the only code segment entry for 64 bit long mode
+    e.pointer_low = pointer & 0xFFFF;
+    e.pointer_middle = (pointer >> 16) & 0xFFFF;
+    e.pointer_high = (pointer >> 32) & 0xFFFFFFFF;
+    e.options = IdtEntryOptions(true);
+    e.always_0 = 0;
+
+    return e;
+}
+
+IdtEntry idt[16] ;
+
+struct IdtSizeAddress {
+    u16 size_minus_1;
+    u64 address;
+} __attribute__((packed)) ;
+
+//extern "C" void* divide_by_zero_handler(void *rsp) {
+//    ScreenPrinter p;
+//    p.format("ZERO DIVISION\n");
+//
+//    return rsp;
+//}
+
+extern "C" void handle_interrupt();
+
+void configIDT() {
+    idt[0] = make_entry((u64)handle_interrupt); // zero division
+    idt[3] = make_entry((u64)handle_interrupt); // breakpoint
+
+    IdtSizeAddress idt_size_address ;
+    idt_size_address.size_minus_1 = sizeof(idt) - 1;
+    idt_size_address.address = (u64)idt;
+
+    __asm__ ("lidt %0"
+            :
+            : "m" (idt_size_address)
+            );
+}
+
+/**
  * kmain
  * Kernel entry point
  */
 extern "C" void kmain(void *multiboot2_info_ptr) {
     // run constructors of global objects. This could be run from long_mode_init.S
     callGlobalConstructors();
+
+    // configure Interrupt Descriptor Table
+    configIDT();
 
     // print hello message to the user
     ScreenPrinter p;
@@ -69,4 +166,5 @@ extern "C" void kmain(void *multiboot2_info_ptr) {
     mb2.print(p);
 
     //test_kstd(p);
+    test_idt();
 }
---
+++ src/types.h
@@ -0,0 +1,4 @@
+using u8    = unsigned char;
+using u16   = unsigned short;
+using u32   = unsigned int;
+using u64   = unsigned long long;
