760188a75dcd237edea927b0f1373ed31e84550c
Mateusz Midor <3demaniac@gmail.com>
2017-06-20 08:41:11 +0200

    Added Port class

---
+++ src/Port.h
@@ -0,0 +1,50 @@
+/**
+ *   @file: Port.h
+ *
+ *   @date: Jun 20, 2017
+ * @author: Mateusz Midor
+ */
+
+#ifndef SRC_PORT_H_
+#define SRC_PORT_H_
+
+#include "types.h"
+
+/**
+ * @class   Port
+ * @brief   Just a base class for Port8bit, Port8bitSlow, Port16bit and Port32bit
+ */
+class Port {
+protected:
+    Port(u16 port_number);
+    u16 port_number;
+};
+
+class Port8bit : protected Port {
+public:
+    Port8bit(u16 port_number);
+    void write(u8 data) const;
+    u8 read() const;
+};
+
+class Port8bitSlow : protected Port8bit {
+public:
+    Port8bitSlow(u16 port_number);
+    void write(u8 data) const;
+};
+
+class Port16bit : protected Port {
+public:
+    Port16bit(u16 port_number);
+    void write(u16 data) const;
+    u16 read() const;
+};
+
+class Port32bit : protected Port {
+public:
+    Port32bit(u32 port_number);
+    void write(u32 data) const;
+    u32 read() const;
+};
+
+#endif /* SRC_PORT_H_ */
