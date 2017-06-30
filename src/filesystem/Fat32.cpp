c09eb0aac17ee9282bd608ed0d47cfeec6efea4f
Mateusz Midor <3demaniac@gmail.com>
2017-06-30 13:32:22 +0200

    Added Fat32 demo support for reading files that span multiple clusters

--- src/filesystem/Fat32.cpp
+++ src/filesystem/Fat32.cpp
@@ -49,7 +49,8 @@ void Fat32::print_directory_entries(const BiosParameterBlock32& bpb, u32 partiti
 
     printer.format("Files:\n");
 
-    for (const auto& e : entries) {
+    for (u8 i = 4; i < entries.size(); i++) {
+        auto& e = entries[i];
         if (e.name[0] == '\0')
             break; // no more entries
 
@@ -64,17 +65,41 @@ void Fat32::print_directory_entries(const BiosParameterBlock32& bpb, u32 partiti
             printer.format("[%]\n", full_name);
             continue;
         }
-
+//        printer.format("% - %B\n", full_name, e.size);
+//        continue;
         if (e.size  == 0) {
             printer.format("% - [EMPTY]\n", full_name);
             continue;
         }
-        u32 file_cluster = e.first_cluster_hi << 16 | e.first_cluster_lo;
-        u32 file_sector = data_start +  bpb.sectors_per_cluster * (file_cluster - 2);
-        u8 buff[512];
-        hdd.read28(file_sector, buff, sizeof(buff));
-        buff[e.size-1] = '\0';
-        printer.format("% - \"%\"\n", full_name, (char*)buff);
+
+
+        // HERE WE TRY READ FILE CONTENTS
+        s32 remaining_size = e.size;
+        u32 first_file_cluster = e.first_cluster_hi << 16 | e.first_cluster_lo;
+        s32 next_file_cluster = first_file_cluster;
+
+        u8 buffer[513]; // last one for \0
+        u8 fat_buffer[512];
+
+        while (remaining_size > 0) {
+            u32 file_sector = data_start + bpb.sectors_per_cluster * (next_file_cluster -2);
+            s32 sector_offset = 0;
+
+            for (; remaining_size > 0; remaining_size -= 512) {
+                hdd.read28(file_sector + sector_offset, buffer, 512);
+                buffer[remaining_size > 512 ? 512 :remaining_size] = '\0';
+                printer.format("%", (char*) buffer);
+                if (++sector_offset > bpb.sectors_per_cluster)
+                    break;
+
+            }
+
+            u32 fat_sector_for_current_cluster = next_file_cluster / (512 / sizeof(u32));
+            hdd.read28(fat_start + fat_sector_for_current_cluster, fat_buffer, 512);
+            u32 fat_offset_in_sector_for_current_cluster = next_file_cluster % (512 / sizeof(u32));
+            next_file_cluster = ((u32*)&fat_buffer)[fat_offset_in_sector_for_current_cluster] & 0x0FFFFFFF;
+        }
+
     }
     printer.format("\n");
 }
--- src/types.h
+++ src/types.h
@@ -2,6 +2,7 @@ using s8    = signed char;
 using u8    = unsigned char;
 using s16   = signed short;
 using u16   = unsigned short;
+using s32   = signed int;
 using u32   = unsigned int;
 using s64   = signed long long;
 using u64   = unsigned long long;
