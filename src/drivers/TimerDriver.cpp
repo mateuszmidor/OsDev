17b9c05ea7c412005bc739b0eb03f3ad4b78dd02
Mateusz Midor <3demaniac@gmail.com>
2017-07-23 18:29:15 +0200

    Added TimerDriver::set_hz

--- src/drivers/TimerDriver.cpp
+++ src/drivers/TimerDriver.cpp
@@ -22,4 +22,19 @@ cpu::CpuState* TimerDriver::on_interrupt(cpu::CpuState* cpu_state) {
 void TimerDriver::set_on_tick(const TimerEvent &event) {
     on_tick = event;
 }
+
+/**
+ * @brief   Set PIC interrupt frequency
+ * @param   hz Frequency in range 18 - 1'193'182 [Hz]
+ * @ref     http://wiki.osdev.org/Programmable_Interval_Timer#Channel_0
+ *          http://kernelx.weebly.com/programmable-interval-timer.html
+ */
+void TimerDriver::set_hz(u16 hz) {
+    const u8 MODE       = 0x36; // channel 0 + lobyte/hibyte access + square wave generator + 16-bit binary mode
+    const u32 DIVISOR   = 1193180 / hz;
+    pit_cmd.write(MODE);
+    pit_channel_0.write(DIVISOR & 0xFF);
+    pit_channel_0.write(DIVISOR >> 8);
+}
+
 } /* namespace drivers */
--- src/drivers/TimerDriver.h
+++ src/drivers/TimerDriver.h
@@ -23,9 +23,14 @@ public:
     static s16 handled_interrupt_no();
     cpu::CpuState* on_interrupt(cpu::CpuState* cpu_state) override;
     void set_on_tick(const TimerEvent &event);
+    void set_hz(u16 hz);
 
 private:
     TimerEvent on_tick = [](cpu::CpuState* cpu_state) { return cpu_state; };
+    Port8bit pit_channel_0  {0x40};
+    Port8bit pit_channel_1  {0x41};
+    Port8bit pit_channel_2  {0x42};
+    Port8bit pit_cmd        {0x43};
 };
 
 } /* namespace drivers */
