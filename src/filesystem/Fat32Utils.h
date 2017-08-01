c98f056dbd45cb3aeab0b3f2375f193f0eb10aa8
Mateusz Midor <3demaniac@gmail.com>
2017-08-01 11:24:45 +0200

    Added Fat32Utils::normalize_path

--- src/filesystem/Fat32Utils.h
+++ src/filesystem/Fat32Utils.h
@@ -14,6 +14,7 @@ namespace filesystem {
 
 class Fat32Utils {
 public:
+    static kstd::string normalize_path(const kstd::string& unix_path);
     static kstd::string make_8_3_filename(const kstd::string& filename);
     static void make_8_3_space_padded_filename(const kstd::string& filename, kstd::string& name, kstd::string& ext);
 };
