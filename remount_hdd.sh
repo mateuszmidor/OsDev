199fb2f726b44381d5bb6448b6829ac6b14c0159
Mateusz Midor <3demaniac@gmail.com>
2017-09-12 12:14:15 +0200

    remount_hdd.sh mounts the hdd.vdi with perms to all uses

--- remount_hdd.sh
+++ remount_hdd.sh
@@ -4,9 +4,9 @@ sudo umount p1
 sudo umount p2
 mkdir -p p1
 mkdir -p p2
-modprobe nbd max_part=16 		# they say max_part=16 is crutial
+modprobe nbd max_part=16 		        # they say max_part=16 is crutial
 qemu-nbd -d /dev/nbd0
-qemu-nbd -c /dev/nbd0 build/hdd.vdi 	# attach /dev/nbd0 to hdd.vdi
-partprobe 				# to discover partitions on nbd0
-sudo mount -o,rw /dev/nbd0p1 p1
-sudo mount -o,rw /dev/nbd0p2 p2
+qemu-nbd -c /dev/nbd0 build/hdd.vdi     # attach /dev/nbd0 to hdd.vdi
+partprobe 				                # to discover partitions on nbd0
+sudo mount -o,rw -ouser,umask=0000 /dev/nbd0p1 p1
+sudo mount -o,rw -ouser,umask=0000 /dev/nbd0p2 p2
