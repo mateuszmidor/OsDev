141d5a966cb69bde4418f8b84886e15643d2c5b7
Mateusz Midor <3demaniac@gmail.com>
2017-10-05 20:14:27 +0200

    Workaround for lscpu page fault at 2054MB fault; to investigate

--- src/utils/terminal/cmds/lscpu.cpp
+++ src/utils/terminal/cmds/lscpu.cpp
@@ -12,7 +12,8 @@ namespace cmds {
 
 void lscpu::run() {
     utils::CpuInfo cpu_info;
-    env->printer->format("CPU: % @ %MHz, %\n", cpu_info.get_vendor(), cpu_info.get_peak_mhz(), cpu_info.get_multimedia_extensions().to_string());
+    kstd::string multimedia_extension_string = cpu_info.get_multimedia_extensions().to_string();
+    env->printer->format("CPU: % @ %MHz, %\n", cpu_info.get_vendor(), cpu_info.get_peak_mhz(), multimedia_extension_string);
 }
 
 } /* namespace cmds */
