4a7683c0141642a8955a4853d328b7b998b8b672
Mateusz Midor <3demaniac@gmail.com>
2017-10-24 11:01:04 +0200

    ls Terminal command show details if given file path

--- elfs/terminal/cmds/ls.cpp
+++ elfs/terminal/cmds/ls.cpp
@@ -19,12 +19,23 @@ void ls::run() {
     else
         path = make_absolute_filename(env->cmd_args[1]);
 
-    int fd = syscalls::open(path.c_str());
-    if (fd < 0) {
+
+    // check exists
+    struct stat s;
+    if (syscalls::stat(path.c_str(), &s) == -1) {
         env->printer->format("ls: path '%' does not exist\n", path);
         return;
     }
 
+    // ls file
+    if (s.st_mode == S_IFREG) {
+        env->printer->format("% - %B\n", env->cmd_args[1], s.st_size);
+        return;
+    }
+
+
+    // ls directory
+    int fd = syscalls::open(path.c_str());
     u32 MAX_ENTRIES = 128; // should there be more in a single dir?
     FsEntry* entries = new FsEntry[MAX_ENTRIES];
 
@@ -36,5 +47,6 @@ void ls::run() {
             env->printer->format("% - %B\n", entries[i].name, entries[i].size);
 
     delete[] entries;
+    syscalls::close(fd);
 }
 } /* namespace cmds */
