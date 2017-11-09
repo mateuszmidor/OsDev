f877b234ac3d4332853ece369d70bed8f6456d17
Mateusz Midor <3demaniac@gmail.com>
2017-11-09 15:11:02 +0100

    Added global constructors runner in user space _start.h

--- user/ustd/src/_start.h
+++ user/ustd/src/_start.h
@@ -8,12 +8,26 @@
 #ifndef ELFS__START_H_
 #define ELFS__START_H_
 
+typedef void (*Constructor)();
+extern "C" Constructor start_ctors;
+extern "C" Constructor end_ctors;
+
+/**
+ * @brief   Call the global constructors
+ * @note    start_ctors and end_ctors defined in linker.ld
+ */
+extern "C" void run_ctors() {
+    for (Constructor* c = &start_ctors; c != &end_ctors; c++)
+        (*c)();
+}
+
 /**
  * @brief   Beacuse this is a -nostdlib binary, we need to provide "_start" symbol for the system to know where to start execution
  */
 __asm__ __volatile__(
     ".global _start     ;"
     "_start:            ;"
+    "call run_ctors     ;"
     "call main          ;"
     "mov %rax, %rdi     ;"  // error code
     "mov $231, %rax     ;"  // syscall 231: exit_group
