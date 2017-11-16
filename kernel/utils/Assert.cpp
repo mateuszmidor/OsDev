a1efa0f0703081a2a3b71c9ac1945be633c3d026
Mateusz Midor <3demaniac@gmail.com>
2017-11-16 09:35:12 +0100

    Added phobos_assert()

---
+++ kernel/utils/Assert.cpp
@@ -0,0 +1,35 @@
+/**
+ *   @file: Assert.cpp
+ *
+ *   @date: Nov 16, 2017
+ * @author: Mateusz Midor
+ */
+
+#include "Assert.h"
+
+namespace utils {
+
+void phobos_assert(bool condition, const char* msg) {
+    if (condition)
+        return;
+
+    const u16 RED_ON_WHITE = (15 << 12) | (4 << 8);
+
+    u16* vga = (u16*)0xFFFFFFFF800b8000;
+    while (*msg) {
+        *vga =  *msg | RED_ON_WHITE;
+        vga++;
+        msg++;
+    }
+
+    const char* system_halt = " [System Halted] ";
+    while (*system_halt) {
+        *vga =  *system_halt | RED_ON_WHITE;
+        vga++;
+        system_halt++;
+    }
+
+    // halt the kernel
+    asm volatile ("cli; hlt");
+}
+} /* namespace utils */
---
+++ kernel/utils/Assert.h
@@ -0,0 +1,19 @@
+/**
+ *   @file: Assert.h
+ *
+ *   @date: Nov 16, 2017
+ * @author: Mateusz Midor
+ */
+
+#ifndef KERNEL_UTILS_ASSERT_H_
+#define KERNEL_UTILS_ASSERT_H_
+
+#include "types.h"
+
+namespace utils {
+
+void phobos_assert(bool condition, const char* msg);
+
+} /* namespace utils */
+
+#endif /* KERNEL_UTILS_ASSERT_H_ */
