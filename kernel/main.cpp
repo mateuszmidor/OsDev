483e4377d09f9f80d38fd7f6b114754e30d2b008
Mateusz Midor <3demaniac@gmail.com>
2017-12-04 10:45:09 +0100

    Removed VESA related code from main.cpp

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -72,82 +72,10 @@ PageFaultHandler        page_fault;
 
 const u32               PIT_FREQUENCY_HZ = 20;
 
-Key last_key;
 TaskList counter_sleep_wl;
 
-struct mode_info_block_t {
-    uint16_t mode_attributes;
-    uint8_t window_a_attributes;
-    uint8_t window_b_attributes;
-    uint16_t window_granularity;
-    uint16_t window_size;
-    uint16_t segment_window_a;
-    uint16_t segment_window_b;
-    uint16_t window_position_function[2];
-    uint16_t pitch; //Bytes per scan line
 
-    //OEM modes
-
-    uint16_t width;
-    uint16_t height;
-    uint8_t width_char;
-    uint8_t height_char;
-    uint8_t memory_planes;
-    uint8_t bpp;
-    uint8_t banks;
-    uint8_t memory_model;
-    uint8_t bank_size;
-    uint8_t image_pages;
-    uint8_t reserved;
-
-    //VBE 1.2+
-
-    uint8_t red_mask_size;
-    uint8_t red_mask_position;
-    uint8_t green_mask_size;
-    uint8_t green_mask_position;
-    uint8_t blue_mask_size;
-    uint8_t blue_mask_position;
-    uint8_t reserved_mask_size;
-    uint8_t reserved__mask_position;
-    uint8_t directcolor_attributes;
-
-    //VBE 2.0+
-
-    uint32_t linear_video_buffer;         //LFB (Linear Framebuffer) address
-    uint16_t offscreen_memory[2];
-    uint16_t offscreen_memory_size;
-
-    uint16_t bytes_per_scan_line;
-    uint8_t number_of_images_banked;
-    uint8_t number_of_images_linear;
-    uint8_t linear_red_mask_size;
-    uint8_t linear_red_mask_position;
-    uint8_t linear_green_mask_size;
-    uint8_t linear_green_mask_position;
-    uint8_t linear_blue_mask_size;
-    uint8_t linear_blue_mask_position;
-    uint8_t linear_reserved_mask_size;
-    uint8_t linear_reserved_mask_position;
-    uint32_t maximum_pixel_clock;
-    uint8_t reserved_2[190];
-} __attribute__((packed));
-
-
-void get_vesa_info() {
-    size_t enabled_addr = 0xFFFFFFFF80000000 + 0x9039C;
-    bool is_enabled = static_cast<bool>(*reinterpret_cast<uint32_t*>(enabled_addr));
-
-    size_t info_addr = 0xFFFFFFFF80000000 + 0x903A0;
-    mode_info_block_t* b = reinterpret_cast<mode_info_block_t*>(info_addr);
-    klog.format("VESA: enabled: %, mode: %x%x% bpp\n",
-            is_enabled,
-            b->width,
-            b->height,
-            b->bpp
-    );
-}
-
+Key last_key;
 VfsEntryPtr keyboard_vfe;
 void handle_keyboard_interrupt(const Key &key) {
     if (key != Key::INVALID) {
@@ -299,8 +227,6 @@ extern "C" void kmain(void *multiboot2_info_ptr) {
     // 10. configure vga text mode
     if (auto vga_drv = driver_manager.get_driver<VgaDriver>())
         vga_drv->set_text_mode_90_30();
-
-    get_vesa_info();
 
     // 11. start multitasking
     task_manager.add_task(TaskFactory::make_kernel_task(task_init, "init"));
