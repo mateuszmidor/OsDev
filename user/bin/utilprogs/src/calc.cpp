ab675698b633e69c0fde4664e36ba69215b7eaa3
Mateusz Midor <3demaniac@gmail.com>
2018-01-04 08:21:12 +0100

    Refactored userspace calc

--- user/bin/utilprogs/src/calc.cpp
+++ user/bin/utilprogs/src/calc.cpp
@@ -12,6 +12,12 @@
 
 using namespace ustd;
 
+string build_formula(int argc, char* argv[]) {
+    string formula;
+    for (int i = 1; i < argc; i++)
+        formula += argv[i];
+    return std::move(formula);
+}
 
 /**
  * @brief   Entry point
@@ -19,25 +25,27 @@ using namespace ustd;
  */
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cout::print("calc: please speficy formula like 2 + 4 * 5\n");
+        cout::print("calc: please speficy formula like 2 + 4 * 5. use sin/cos/sqrt/abs\n");
         return 1;
     }
 
-    string formula {};
-    for (int i = 1; i < argc; i++)
-        formula += argv[i];
+    string formula = build_formula(argc, argv);
 
     rpn::Calculator c;
-    if (auto result = c.parse(formula))
-        void(0); // we are good
-    else
-        cout::format("ERROR: %\n", result.error_msg);
+    auto parse_result = c.parse(formula);
+    if (!parse_result) {
+        cout::format("calc: parse error: %\n", parse_result.error_msg);
+        return 1;
+    }
 
-    if (auto result = c.calc())
-        cout::format("RESULT: %\n", result.value);
-    else
-        cout::format("ERROR: %\n", result.error_msg);
+    c.define("e", 2.718281828459);
+    auto calc_result = c.calc();
+    if (!calc_result) {
+        cout::format("calc: calculation error: %\n", calc_result.error_msg);
+        return 1;
+    }
 
+    cout::format("RESULT: %\n", calc_result.value);
     return 0;
 }
 
