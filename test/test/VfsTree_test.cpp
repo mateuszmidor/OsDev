19442af0f80f041e6bf07ad9790f89880f24e012
Mateusz Midor <3demaniac@gmail.com>
2018-08-14 11:25:11 +0200

    Reenabled and adjusted tests for new VfsTree interface

--- test/test/VfsTree_test.cpp
+++ test/test/VfsTree_test.cpp
@@ -19,7 +19,6 @@ using namespace filesystem;
 class VfsTreeTest : public ::testing::Test {
 protected:
     VfsTree tree;
-    GlobalFileDescriptor ROOT_FD;
 
     VfsRamDirectoryEntryPtr mkdir(const char name[]) {
         return std::make_shared<VfsRamDirectoryEntry>(name);
@@ -158,54 +157,10 @@ TEST_F(VfsTreeTest, test_remove_in_mountpoint) {
     auto fd = tree.create("/HOME/images", true);
     ASSERT_TRUE(fd);
     ASSERT_TRUE(tree.exists("/HOME/images"));
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.remove("/HOME/images"));
     ASSERT_FALSE(tree.exists("/HOME/images"));
-}
-
-/**************************************************************************
- * VfsTree::open
- *************************************************************************/
-TEST_F(VfsTreeTest, test_open_existent) {
-    ASSERT_TRUE(tree.open("/"));
-}
-
-TEST_F(VfsTreeTest, test_open_nonexistent) {
-    ASSERT_FALSE(tree.open("/nonexistent"));
-}
-
-TEST_F(VfsTreeTest, test_open_deeplyhidden) {
-    auto home = mkdir("home");
-    auto images = mkdir("images");
-    auto cars = mkdir("cars");
-    auto volvo = mkdir("volvo");
-    home->attach_entry(images);
-    images->attach_entry(cars);
-    cars->attach_entry(volvo);
-    tree.attach(home, "/");
-    ASSERT_TRUE(tree.open("/home/images/cars/volvo"));
-}
-
-/**************************************************************************
- * VfsTree::close
- *************************************************************************/
-TEST_F(VfsTreeTest, test_close_opened) {
-    auto fd = tree.open("/");
-    ASSERT_TRUE(tree.close(fd.value));
-}
-
-TEST_F(VfsTreeTest, test_close_closed) {
-    auto fd = tree.open("/");
-    tree.close(fd.value);
-    ASSERT_FALSE(tree.close(fd.value));
-}
-
-TEST_F(VfsTreeTest, test_close_neveropened) {
-    auto fd = 42;
-    tree.close(fd);
-    ASSERT_FALSE(tree.close(fd));
 }
 
 /**************************************************************************
@@ -252,7 +207,6 @@ TEST_F(VfsTreeTest, test_copy_within_different_mountpoints) {
     ASSERT_TRUE(tree.attach(std::make_shared<VfsRamMountPoint>("IMAGES"), "/"));
     auto fd = tree.create("/HOME/photo.jpg", false);
     ASSERT_TRUE(fd);
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.copy("/HOME/photo.jpg", "/IMAGES/photo.jpg"));
@@ -316,7 +270,6 @@ TEST_F(VfsTreeTest, test_move_within_mountpoint_same_level) {
     ASSERT_TRUE(tree.attach(std::make_shared<VfsRamMountPoint>("HOME"), "/"));
     auto fd = tree.create("/HOME/photo.jpg", false);
     ASSERT_TRUE(fd);
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.move("/HOME/photo.jpg", "/HOME/picture.jpg"));
@@ -330,7 +283,6 @@ TEST_F(VfsTreeTest, test_move_within_mountpoint_different_level) {
     ASSERT_TRUE(tree.create("/HOME/images", true));
     auto fd = tree.create("/HOME/photo.jpg", false);
     ASSERT_TRUE(fd);
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.move("/HOME/photo.jpg", "/HOME/images/photo.jpg"));
@@ -344,7 +296,6 @@ TEST_F(VfsTreeTest, test_move_within_mountpoint_different_level_to_dir) {
     ASSERT_TRUE(tree.create("/HOME/images", true));
     auto fd = tree.create("/HOME/photo.jpg", false);
     ASSERT_TRUE(fd);
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.move("/HOME/photo.jpg", "/HOME/images/"));
@@ -358,7 +309,6 @@ TEST_F(VfsTreeTest, test_move_within_different_mountpoints) {
     ASSERT_TRUE(tree.attach(std::make_shared<VfsRamMountPoint>("IMAGES"), "/"));
     auto fd = tree.create("/HOME/photo.jpg", false);
     ASSERT_TRUE(fd);
-    ASSERT_TRUE(tree.close(fd.value));
 
     // test
     ASSERT_TRUE(tree.move("/HOME/photo.jpg", "/IMAGES/photo.jpg"));
