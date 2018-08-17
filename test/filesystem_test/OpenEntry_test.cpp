469a75f3f0bf47998862a0b341f72f9032927793
Mateusz Midor <3demaniac@gmail.com>
2018-08-17 10:35:55 +0200

    Added OpenEntry_test.cpp

---
+++ test/filesystem_test/OpenEntry_test.cpp
@@ -0,0 +1,80 @@
+/**
+ *   @file: OpenEntry_test.cpp
+ *
+ *   @date: Aug 17, 2018
+ * @author: Mateusz Midor
+ */
+
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "OpenEntry.h"
+#include "VfsCachedEntry.h"
+#include "VfsRamDummyFileEntry.h"
+
+using namespace filesystem;
+
+class OpenEntryTest : public ::testing::Test {
+protected:
+    VfsCachedEntryPtr entry;
+
+//    VfsRamDirectoryEntryPtr mkdir(const char name[]) {
+//        return std::make_shared<VfsRamDirectoryEntry>(name);
+//    }
+//
+//    std::shared_ptr<VfsRamDummyFileEntry> mkfile(const char name[]) {
+//        return std::make_shared<VfsRamDummyFileEntry>(name);
+//    }
+//
+//    std::shared_ptr<VfsRamMountPoint> mkmountpoint(const char name[]) {
+//        return std::make_shared<VfsRamMountPoint>(name);
+//    }
+
+    void SetUp() override {
+        entry = std::make_shared<VfsCachedEntry>(std::make_shared<VfsRamDummyFileEntry>("picture.jpg"));
+    }
+
+};
+
+
+TEST_F(OpenEntryTest, test_constructor_increments_open_count) {
+    // setup
+    ASSERT_EQ(entry->open_count, 0);
+
+    // test
+    OpenEntry open1(entry);
+    ASSERT_EQ(entry->open_count, 1);
+
+    OpenEntry open2(entry);
+    ASSERT_EQ(entry->open_count, 2);
+}
+
+TEST_F(OpenEntryTest, test_destructor_decrements_open_count) {
+    // setup
+    ASSERT_EQ(entry->open_count, 0);
+    OpenEntry open1(entry);
+    OpenEntry open2(entry);
+    ASSERT_EQ(entry->open_count, 2);
+
+    // test
+    open1.~OpenEntry();
+    ASSERT_EQ(entry->open_count, 1);
+
+    open2.~OpenEntry();
+    ASSERT_EQ(entry->open_count, 0);
+}
+
+TEST_F(OpenEntryTest, test_assignment_decrements_open_count) {
+    // setup
+    ASSERT_EQ(entry->open_count, 0);
+    OpenEntry open1(entry);
+    OpenEntry open2(entry);
+    ASSERT_EQ(entry->open_count, 2);
+
+    // test
+    open1 = {};
+    ASSERT_EQ(entry->open_count, 1);
+
+    open2 = {};
+    ASSERT_EQ(entry->open_count, 0);
+}
