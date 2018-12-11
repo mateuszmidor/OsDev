9995d4361043450a52cd65e5325e7bf5cea6eade
Mateusz Midor <3demaniac@gmail.com>
2018-12-11 17:54:23 +0100

    Fix rungdb.sh

--- rungdb.sh
+++ rungdb.sh
@@ -3,4 +3,4 @@
 HDD=build/hdd.vdi
 ISO=build/kernel/phobos-x86_64.iso
 
-qemu-system-x86_64 -net nic,model=pcnet -boot d -hdb ${HDD} -cdrom ${ISO} -s -S -no-reboot # pcnet is AMD am79c973 network chip
+qemu-system-x86_64 -net nic,model=pcnet -boot d -hdb $HDD -cdrom $ISO -s -S -no-reboot # pcnet is AMD am79c973 network chip
